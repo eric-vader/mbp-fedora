29d7e4ec57f259a2ae37ee691583a456415e8e4d
Eric Han <Eric.aka.darth_vader@hotmail.com>
2020-10-21 15:42:04 +0800

    Remove backend

--- backend/generate-key.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ssh-keygen -t rsa -b 4096 -m PEM
-openssl rsa -in id_rsa -outform pem > id_rsa.pem
-openssl rsa -in id_rsa -pubout -outform pem > id_rsa.pub.pem
-mkdir -p ship
-cp id_rsa.pub.pem ship/id_rsa.pub.pem
